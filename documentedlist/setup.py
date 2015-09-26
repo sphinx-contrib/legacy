49d829384f7295ec72ee3c79eb5796fb15e47248
Chintalagiri Shashank <shashank@chintal.in>
2015-09-27 05:25:39 +0530

    Added the :spantolast: and :descend: options.

--- documentedlist/setup.py
+++ documentedlist/setup.py
@@ -11,7 +11,7 @@ requires = ['Sphinx>=0.6']
 
 setup(
     name='sphinxcontrib-documentedlist',
-    version='0.2',
+    version='0.3',
     url='http://bitbucket.org/birkenfeld/sphinx-contrib',
     download_url='http://pypi.python.org/pypi/sphinxcontrib-documentedlist',
     license='BSD',
